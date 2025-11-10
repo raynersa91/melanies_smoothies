db9de48d031de58913d9b509a634fbc26307bf91
RAYNER STEFERSON ARAUJO <134096175+raynersa91@users.noreply.github.com>
2025-11-10 09:25:07 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,8 +11,8 @@ st.write(
   """
 )
 
-name_on_order = st.text_input("Name on Smoothie:")
-st.write("The name on your Smoothie will be", name_on_order)
+#name_on_order = st.text_input("Name on Smoothie:")
+#st.write("The name on your Smoothie will be", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
